91f5b52f3f383e4615f54fff1ab94f4924fb804a
Gael Renê <202408035268@alunos.unifbv.edu.br>
2025-11-15 20:31:42 -0300

    feat: add suporte ao Vercel Analytics

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ const port = process.env.APP_PORT ? Number(process.env.APP_PORT) : 8080;
 
 const app = require("./app");
 const { startCleanupJob } = require("./jobs/cleanup-job");
+const { inject } = require("@vercel/analytics");
 
 try {
   if (!port || isNaN(port)) {
     process.exit(1);
   }
+  
+  // Injeta o Vercel Analytics
+  inject();
+  
   app.listen(port, "0.0.0.0", () => {});
   console.log(`Server is running on port ${port}`);
   
